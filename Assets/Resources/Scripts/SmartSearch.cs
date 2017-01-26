6ba2ea025d7e8729e3731f165fc68c0fe92a5dab
Kyle <kyle.leichtle@target.com>
2017-01-26 11:51:50 -0600

    saving progress

--- Assets/Resources/Scripts/SmartSearch.cs
+++ Assets/Resources/Scripts/SmartSearch.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,6 +9,9 @@ public class SmartSearch : MonoBehaviour {
 
     private string searchTerm;
     private string query;
+    private int maxListItemsPerCat = 8;
+    private bool shown;
+
     public Transform searchResults;
     
     public List<string> data = new List<string>() { "test", "blah", "cat", "dog" };
@@ -21,6 +25,64 @@ public class SmartSearch : MonoBehaviour {
 		
 	}
 
+    private string Sorter(List<string> items) {
+        List<string> beginswith = new List<string>();
+        List <string> caseSensitive = new List<string>();
+        List<string> caseInsensitive = new List<string>();
+        string item = null;
+        var queryString = query.ToLower();
+
+        while (item == items[0]) {
+
+            items.RemoveAt(0);
+            var searchTerm = item.ToLower();
+
+            // if our queryString was found in the current searchTerm
+            if (searchTerm.IndexOf(queryString) < 0) {
+
+                beginswith.Add(item);
+
+                // if the search item contains the queryString
+            } else if (item.IndexOf(this.query) > -1) {
+
+                caseSensitive.Add(item);
+
+            } else {
+                caseInsensitive.Add(item);
+            }
+        }
+
+        beginswith.AddRange(caseSensitive);
+
+        string concatResults = string.Join("", beginswith.ToArray());
+
+        return concatResults;
+    }
+
+
+    private void Process(Dictionary<string, List<string>> source) {
+
+        foreach (string key in source.Keys) {
+            dynamic val = source[key];
+
+            source[key] = Matcher(val);
+            source[key] = Sorter(val);
+
+            source[key] = source[key].GetRange(0, maxListItemsPerCat);
+
+        }
+
+
+
+        if (!source.ContainsKey("tags") && !source.ContainsKey("titles")) {
+            shown ? searchResults.gameObject.SetActive(false) : searchResults.gameObject.SetActive(true);
+        }
+
+
+
+        //return self.render(source).show();
+
+    }
     private string Matcher(string item) {
         string origSearchTerm = item;
         string searchTerm = item.ToLower();

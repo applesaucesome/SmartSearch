237a597e4dce08a5465ecd562affdc10f0239a25
Kyle <kyle.leichtle@target.com>
2017-01-26 09:52:22 -0600

    starting building out matcher functions

--- Assets/Resources/Scripts/SmartSearch.cs
+++ Assets/Resources/Scripts/SmartSearch.cs
@@ -6,8 +6,10 @@ using UnityEngine.UI;
 [ExecuteInEditMode]
 public class SmartSearch : MonoBehaviour {
 
+    private string searchTerm;
+    private string query;
     public Transform searchResults;
-
+    
     public List<string> data = new List<string>() { "test", "blah", "cat", "dog" };
 	// Use this for initialization
 	void Start () {
@@ -19,6 +21,48 @@ public class SmartSearch : MonoBehaviour {
 		
 	}
 
+    private string Matcher(string item) {
+        string origSearchTerm = item;
+        string searchTerm = item.ToLower();
+        string query = this.query.ToLower();
+
+        if(query.Length > 1 && searchTerm.IndexOf(query[0]) > -1) {
+
+            int i = 0;
+            return SearchIt(origSearchTerm, query, 0, i);
+
+        } else if(searchTerm.IndexOf(query) > -1) {
+
+            return searchTerm;
+
+        } else {
+            return null;
+        }
+
+        
+    }
+    private string SearchIt(string origSearchTerm, string query, int indexFoundAt, int i) {
+
+        searchTerm = searchTerm.Substring(indexFoundAt);
+
+        if(searchTerm.IndexOf(query[i]) > -1) {
+
+            int foundItAt = searchTerm.IndexOf(query[i]);
+
+            i++;
+            return SearchIt(origSearchTerm, query, indexFoundAt, i);
+
+        } else if (i == query.Length) {
+
+            return origSearchTerm;
+
+        } else {
+            return null;
+        }
+
+        
+
+    }
     public void OnTextChange(string val) {
         Debug.Log("TEXT CHANGED: " + val);
 
